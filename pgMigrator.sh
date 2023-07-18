7c93bbcf097a3643f15080e47f53e41c93996b36
Diego Quispe <72992112+diegoalonsoqj@users.noreply.github.com>
2023-07-18 12:11:50 -0500

    v2.0
    En este script, se han utilizado funciones para mejorar la estructura y reutilizar código. La función get_user_input se utiliza para obtener la entrada del usuario, check_directory para verificar y crear el directorio de respaldo si es necesario, y log_event para registrar eventos en el archivo de registro.

--- pgMigrator.sh
+++ pgMigrator.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
 
-# Pide al usuario la información de la base de datos
-echo "Ingrese el host de la base de datos:"
-read HOST
-echo "Ingrese el nombre de la base de datos:"
-read DB_NAME
-echo "Ingrese el nombre de usuario de la base de datos:"
-read USERNAME
+# Función para la entrada del usuario
+get_user_input() {
+    echo "Ingrese $1:"
+    read var
+    echo $var
+}
+
+# Función para verificar la existencia del directorio
+check_directory() {
+    if [ ! -d "$1" ]
+    then
+        echo "Creando directorio: $1"
+        mkdir -p $1
+    fi
+}
+
+# Función para el registro de eventos
+log_event() {
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] - $1" >> $LOG_FILE
+}
+
+# Inicializar variables con la entrada del usuario
+HOST=$(get_user_input "el host de la base de datos")
+DB_NAME=$(get_user_input "el nombre de la base de datos")
+USERNAME=$(get_user_input "el nombre de usuario de la base de datos")
 echo "Ingrese la contraseña de la base de datos:"
 read -s PASSWORD
-
-# Pregunta por el directorio de respaldo
-echo "Ingrese el directorio donde se almacenarán los respaldos:"
-read BACKUP_DIR
+BACKUP_DIR=$(get_user_input "el directorio donde se almacenarán los respaldos")
+LOG_FILE="/var/log/my_script.log"
 
 # Verifica si el directorio de respaldo existe
-if [ ! -d "$BACKUP_DIR" ]
-then
-    # Crea el directorio si no existe
-    echo "Creando directorio: $BACKUP_DIR"
-    mkdir -p $BACKUP_DIR
-fi
+check_directory $BACKUP_DIR
 
 # Crear una variable para el nombre del archivo
 BACKUP_FILE="${BACKUP_DIR}${DB_NAME}_PROD-$(date '+%Y%m%d_%H%M%S').sql"
 
-# Crear una variable para el archivo log
-LOG_FILE="/var/log/pgMigrator.log"
-
-echo "[$(date '+%Y-%m-%d %H:%M:%S')] - Inicio del respaldo" >> $LOG_FILE
+log_event "Inicio del respaldo"
 
 # Crear el respaldo
 pg_dump -h $HOST -U $USERNAME -W $PASSWORD -F p -b -v -f $BACKUP_FILE $DB_NAME
@@ -36,48 +44,39 @@ pg_dump -h $HOST -U $USERNAME -W $PASSWORD -F p -b -v -f $BACKUP_FILE $DB_NAME
 # Obtener el tamaño del archivo
 BACKUP_SIZE=$(du -h $BACKUP_FILE | cut -f1)
 
-echo "[$(date '+%Y-%m-%d %H:%M:%S')] - Respaldo completado: $BACKUP_FILE (Tamaño: $BACKUP_SIZE)" >> $LOG_FILE
+log_event "Respaldo completado: $BACKUP_FILE (Tamaño: $BACKUP_SIZE)"
 
 # Pregunta al usuario si desea realizar la búsqueda y reemplazo
-echo "¿Desea buscar y reemplazar una cadena de texto en el archivo de respaldo? [s/n]"
-read ANSWER
+ANSWER=$(get_user_input "¿Desea buscar y reemplazar una cadena de texto en el archivo de respaldo? [s/n]")
 
 if [ "$ANSWER" = "s" ]
 then
-    # Pregunta al usuario la cadena de texto a buscar y reemplazar
-    echo "Ingrese la cadena de texto a buscar:"
-    read SEARCH
-    echo "Ingrese la cadena de texto para reemplazar:"
-    read REPLACE
+    SEARCH=$(get_user_input "la cadena de texto a buscar")
+    REPLACE=$(get_user_input "la cadena de texto para reemplazar")
 
-    echo "[$(date '+%Y-%m-%d %H:%M:%S')] - Inicio de la búsqueda y reemplazo" >> $LOG_FILE
+    log_event "Inicio de la búsqueda y reemplazo"
 
     # Realiza la búsqueda y reemplazo en el archivo de respaldo
     sed -i "s/$SEARCH/$REPLACE/g" $BACKUP_FILE
 
-    echo "[$(date '+%Y-%m-%d %H:%M:%S')] - Búsqueda y reemplazo completado" >> $LOG_FILE
+    log_event "Búsqueda y reemplazo completado"
 fi
 
 # Pregunta al usuario si desea restaurar el respaldo
-echo "¿Desea restaurar el respaldo en la base de datos destino? [s/n]"
-read RESTORE_ANSWER
+RESTORE_ANSWER=$(get_user_input "¿Desea restaurar el respaldo en la base de datos destino? [s/n]")
 
 if [ "$RESTORE_ANSWER" = "s" ]
 then
-    # Pregunta al usuario la información de la base de datos destino para restaurar el respaldo
-    echo "Ingrese el host de la base de datos destino:"
-    read DEST_HOST
-    echo "Ingrese el nombre de la base de datos destino:"
-    read DEST_DB_NAME
-    echo "Ingrese el nombre de usuario de la base de datos destino:"
-    read DEST_USERNAME
+    DEST_HOST=$(get_user_input "el host de la base de datos destino")
+    DEST_DB_NAME=$(get_user_input "el nombre de la base de datos destino")
+    DEST_USERNAME=$(get_user_input "el nombre de usuario de la base de datos destino")
     echo "Ingrese la contraseña de la base de datos destino:"
     read -s DEST_PASSWORD
 
-    echo "[$(date '+%Y-%m-%d %H:%M:%S')] - Inicio de restauración" >> $LOG_FILE
+    log_event "Inicio de restauración"
 
     # Restaurar el respaldo en la base de datos destino
     psql -h $DEST_HOST -U $DEST_USERNAME -W $DEST_PASSWORD -d $DEST_DB_NAME -f $BACKUP_FILE
 
-    echo "[$(date '+%Y-%m-%d %H:%M:%S')] - Restauración completada" >> $LOG_FILE
+    log_event "Restauración completada"
 fi
